8e4ad9d722f45b5de5ee40380eccacf4b05ce3a9
iunona.panasenco <iunona.panasenco@peakwork.com>
2023-02-09 11:36:34 +0200

    oop task

---
+++ src/coding/oop/kolea_oop/Game.java
@@ -0,0 +1,7 @@
+package coding.oop.kolea_oop;
+
+public class Game {
+    //create some dummy methods in game
+    //create 2 classes (separately) that would extend game (and override the methods)
+    //create a Main function which would test the polymorph methods
+}
